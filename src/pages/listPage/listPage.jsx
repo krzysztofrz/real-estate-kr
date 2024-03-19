791c44d6f4251574eeded01b424254bcda454cf8
krzysztofrz <34165654+krzysztofrz@users.noreply.github.com>
2024-03-19 12:54:01 +0100

    list basic 25_26__1

--- src/pages/listPage/listPage.jsx
+++ src/pages/listPage/listPage.jsx
@@ -1,9 +1,22 @@
 import "./listPage.scss";
+import { listData } from "../../lib/dumydata.ts";
+import Filter from "../../components/filter/Filter.jsx";
+import Card from "../../components/card/Card.jsx";
 
 const ListPage = () => {
+	const data = listData;
 	return (
 		<div className="listPage">
-			<h1>listPage</h1>
+			<div className="listContainer">
+				<div className="wrapper">
+					<Filter />
+					{data.map((item) => (
+						<Card key={item.id} item={item} />
+					))}
+				</div>
+			</div>
+
+			<div className="mapContainer">map</div>
 		</div>
 	);
 };
