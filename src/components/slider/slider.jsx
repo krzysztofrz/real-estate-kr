3549a731f3f75322367055cdfc616f253e28dbe0
krzysztofrz <34165654+krzysztofrz@users.noreply.github.com>
2024-03-20 11:45:40 +0100

    slider basics 06_06_2

--- src/components/slider/slider.jsx
+++ src/components/slider/slider.jsx
@@ -1,7 +1,31 @@
 import "./slider.scss";
 
-const Slider = () => {
-	return <div className="slider">????</div>;
+const Slider = ({ images }) => {
+	return (
+		<div className="slider">
+			<div className="fullSlider">
+				<div className="arrow">
+					<img src="/arrow.png" alt="" />
+				</div>
+				<div className="imgContainer">
+					<img src={images[0]} alt="" />
+				</div>
+				<div className="arrow">
+					<img src="/arrow.png" className="right" alt="" />
+				</div>
+				<div className="close">X</div>
+			</div>
+
+			<div className="bigImages">
+				<img src={images[0]} alt="" />
+			</div>
+			<div className="smallImages">
+				{images.slice(1).map((image, index) => (
+					<img src={image} alt="" key={index} />
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default Slider;
