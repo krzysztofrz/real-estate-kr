77dc0d47aa284e75093e832c671a6f9d62ed06f9
krzysztofrz <34165654+krzysztofrz@users.noreply.github.com>
2024-03-21 13:10:10 +0100

    czat logic plus responsive 00_01_3

--- src/components/card/chat/chat.jsx
+++ src/components/card/chat/chat.jsx
@@ -1,6 +1,8 @@
 import "./chat.scss";
+import { useState } from "react";
 
 const Chat = () => {
+	const [chat, setChat] = useState(1);
 	return (
 		<div className="chat">
 			<div className="messages">
@@ -62,73 +64,77 @@ const Chat = () => {
 					<p>Lorem ipsum dolor sird sed...</p>
 				</div>
 			</div>
-			<div className="chatBox">
-				<div className="top">
-					<div className="user">
-						<img
-							src="https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-							alt=""
-						/>
-						John Doe
+			{chat && (
+				<div className="chatBox">
+					<div className="top">
+						<div className="user">
+							<img
+								src="https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+								alt=""
+							/>
+							John Doe
+						</div>
+						<span className="close" onClick={() => setChat(!chat)}>
+							X
+						</span>
 					</div>
-					<span className="close">X</span>
+					<div className="center">
+						<div className="chatMessage">
+							<p>
+								Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+								Consectetur, odio!
+							</p>
+							<span>1 hour ago</span>
+						</div>
+						<div className="chatMessage own">
+							<p>
+								Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+								Consectetur, odio!
+							</p>
+							<span>1 hour ago</span>
+						</div>
+						<div className="chatMessage">
+							<p>
+								Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+								Consectetur, odio!
+							</p>
+							<span>1 hour ago</span>
+						</div>
+						<div className="chatMessage">
+							<p>
+								Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+								Consectetur, odio!
+							</p>
+							<span>1 hour ago</span>
+						</div>
+						<div className="chatMessage own">
+							<p>
+								Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+								Consectetur, odio!
+							</p>
+							<span>1 hour ago</span>
+						</div>
+						<div className="chatMessage own">
+							<p>
+								Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+								Consectetur, odio!
+							</p>
+							<span>1 hour ago</span>
+						</div>
+						<div className="chatMessage">
+							<p>
+								Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+								Consectetur, odio!
+							</p>
+							<span>1 hour ago</span>
+						</div>
+					</div>
+					<div className="bottom">
+						<textarea></textarea>
+						<button>Send</button>
+					</div>
 				</div>
-				<div className="center">
-					<div className="chatMessage">
-						<p>
-							Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-							Consectetur, odio!
-						</p>
-						<span>1 hour ago</span>
-					</div>
-					<div className="chatMessage own">
-						<p>
-							Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-							Consectetur, odio!
-						</p>
-						<span>1 hour ago</span>
-					</div>
-					<div className="chatMessage">
-						<p>
-							Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-							Consectetur, odio!
-						</p>
-						<span>1 hour ago</span>
-					</div>
-					<div className="chatMessage">
-						<p>
-							Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-							Consectetur, odio!
-						</p>
-						<span>1 hour ago</span>
-					</div>
-					<div className="chatMessage own">
-						<p>
-							Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-							Consectetur, odio!
-						</p>
-						<span>1 hour ago</span>
-					</div>
-					<div className="chatMessage own">
-						<p>
-							Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-							Consectetur, odio!
-						</p>
-						<span>1 hour ago</span>
-					</div>
-					<div className="chatMessage">
-						<p>
-							Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-							Consectetur, odio!
-						</p>
-						<span>1 hour ago</span>
-					</div>
-				</div>
-				<div className="bottom">
-					<textarea></textarea>
-					<button>Send</button>
-				</div>
-			</div>
+			)}
 		</div>
 	);
 };
